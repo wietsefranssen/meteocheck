99374b9cb7d2c2501f4d3e33d70e6da48b26283b
Wietse Franssen <wietse.franssen@wur.nl>
2025-06-16 14:16:56 +0200

    added support for vairable location table

--- get_vu_data.py
+++ get_vu_data.py
@@ -22,9 +22,9 @@ def get_siteids(shortname):
               """
               cur.execute(query, ())
               rows = cur.fetchall()
-              print("The number of parts: ", cur.rowcount)
-              # for row in rows:
-              #     print(row)
+            #   print("The number of parts: ", cur.rowcount)
+            #   for row in rows:
+            #       print(row)
                 
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
@@ -172,6 +172,134 @@ def get_sensorinfo(siteid, names):
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
 
+def get_sensorinfo_siteid_name_combo(siteid_names_combo):
+ 
+    
+    # Convert siteid_names_combo to a string by removing the [ and ] characters
+    siteid_names_combo = str(siteid_names_combo).replace('[', '').replace(']', '')
+    
+    
+    # Make a string for the siteid_names_combo     
+    # siteid_names_combo_string = get_dbstring(siteid_names_combo)
+
+    """ Retrieve data from the vendors table """
+    config  = load_config()
+    try:
+        with psycopg2.connect(**config) as conn:
+            # with conn.cursor() as cur:
+            with conn.cursor(cursor_factory=RealDictCursor) as cur:
+                query = f"""
+                SELECT id AS sensor_id, unit AS unit_id, name AS sensor_name, aggmethod AS aggmethod, site AS site_id
+                FROM cdr.logvalproviders
+                WHERE (site, name) IN ({siteid_names_combo})
+                """
+                
+                cur.execute(query, ())
+                sensor_info_result = cur.fetchall()
+                if not sensor_info_result:
+                    # print(f"No sensor_id found for site_id: {site_id}")
+                    # print(f"No sensor_id found for site_id: {site_id}, names: {names}")
+                    return None
+                # sensor_ids = [row['sensor_id'] for row in sensor_info_result]
+                unit_ids = [row['unit_id'] for row in sensor_info_result]
+
+                # data_result = cur.fetchall()
+              
+                  # Get sensor units
+                sensor_units = get_sensor_units(cur, unit_ids)  
+
+                # Add sensor units to sensor_info_result
+                for i, row in enumerate(sensor_info_result):
+                    unit_id = row['unit_id']
+                    # Find the corresponding unit from sensor_units
+                    unit_row = next((u for u in sensor_units if u['unit_id'] == unit_id), None)
+                    if unit_row:
+                        sensor_info_result[i]['sensor_units'] = unit_row['unit']
+                    else:
+                        sensor_info_result[i]['sensor_units'] = None  # or some default value
+
+                # Get sensor units
+                sensor_units = get_sensor_units(cur, unit_ids)  
+
+                # Add sensor units to sensor_info_result
+                for i, row in enumerate(sensor_info_result):
+                    unit_id = row['unit_id']
+                    # Find the corresponding unit from sensor_units
+                    unit_row = next((u for u in sensor_units if u['unit_id'] == unit_id), None)
+                    if unit_row:
+                        sensor_info_result[i]['sensor_units'] = unit_row['unit']
+                    else:
+                        sensor_info_result[i]['sensor_units'] = None  # or some default value
+
+                return sensor_info_result
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+def get_check_table(filename='check_table.csv'):
+    """ 
+    This function reads the check_table.csv file and returns a DataFrame with the columns and index names
+    where the value is 'x'.
+    """
+    # Read in check_table.csv file 
+    check_table = pd.read_csv(filename, index_col=0)
+    
+    # Remove any leading or trailing whitespace from the column names
+    check_table.columns = check_table.columns.str.strip()
+    # Remove any leading or trailing whitespace from the index names
+    check_table.index = check_table.index.str.strip()
+    
+    result = []
+    for idx in check_table.index:
+        for col in check_table.columns:
+            if check_table.loc[idx, col] == 'x':
+                result.append({'Station': col, 'Variable': idx})
+
+    result_df = pd.DataFrame(result)
+    return result_df
+
+# def get sensorinfo by site and varname combination
+def get_sensorinfo_by_site_and_varname(check_table):
+    
+    # get sites by selecting all unique values in the 'Station' column of the check_table
+    sites = check_table['Station'].unique().tolist()
+    
+    # Get the siteid and siteid_name from the database
+    siteid, siteid_name = get_siteids(sites)
+    
+    # Match the Station names in check_table with the siteid_name and add a new column 'siteid' integer to check_table as integer values        
+    check_table['siteid'] = check_table['Station'].map(dict(zip([s[1] for s in siteid_name], siteid)))
+
+    # Conert NaN values in the siteid column to -9999 and make in integer
+    check_table['siteid'] = check_table['siteid'].fillna(-9999).astype(int)
+    
+    # siteid and Variable columns from check_table
+    siteid_varname = check_table[['siteid', 'Variable']].drop_duplicates()
+    
+    # make a list of tuples (siteid, varname) from siteid_varname
+    siteid_varname = [tuple(x) for x in siteid_varname.to_numpy()]
+    
+    sensor_info = get_sensorinfo_siteid_name_combo(siteid_varname)
+    # print(sensor_info)
+    # add a sitename column to sensor_info by using siteid_name as mapping (column 0 is site_id, and column 1 is site_name)
+    for i, row in enumerate(sensor_info):
+        site_id = row['site_id']
+        # Find the corresponding site name from siteid_name
+        site_row = next((s for s in siteid_name if s[0] == site_id), None)
+        if site_row:
+            sensor_info[i]['site_name'] = site_row[1]
+        else:
+            sensor_info[i]['site_name'] = None
+    
+    # Add a fullname column to sensor_info by combining site_name and sensor_name
+    for i, row in enumerate(sensor_info):
+        site_name = row['site_name']
+        sensor_name = row['sensor_name']
+        # Combine site_name and sensor_name
+        sensor_info[i]['fullname'] = f"{site_name}_{sensor_name}"
+        
+    return sensor_info
+
 
 
 if __name__ == '__main__':
@@ -182,72 +310,30 @@ if __name__ == '__main__':
     # Define start_date as 7 days before today
     start_dt = (pd.to_datetime('today') - pd.DateOffset(days=7)).strftime('%Y-%m-%d')
     start_dt, end_dt = fix_start_end_dt(start_dt=start_dt, end_dt=end_dt)
-
-
-    sites = ['ALB_MS', 'ALB_RF','ZEG_RF','MOB_X1','MOB_X2','LAW_MS','DEM_NT','ASD_MP','ANK_PT','ZEG_MP','ZEG_PT','ILP_PT','WRW_SR','WRW_OW','ZEG_MOB']
-    # sites='ALB_MS'
-          
-    varname = ['MT1_SWIN_1_H_180','MT1_PAR_1_H_180']
-    varname = ['MT1_SWIN_1_H_180']
-    # varname = ['SWOUT']
-    # read checks.csv file to get the internal varname related to the database varname
-    checks_df = pd.read_csv('checks.csv')
     
-    # select only the row with name 'SWIN'
-    # checks_df = checks_df[checks_df['variable'].str.contains('SWIN')]
-    print(checks_df)
+    # Get the check_table
+    check_table = get_check_table()
 
-    # make a dict of column names as location and values as variable names from the checks_df and skip the first column
-    checks_dict = {row.iloc[0]: row.iloc[1:] for index, row in checks_df.iterrows()}
-
-    # def get sensorinfo by site and varname combination
-    def get_sensorinfo_by_site_and_varname(sites, varname):
-    
-        # Get the siteid and siteid_name from the database
-        siteid, siteid_name = get_siteids(sites)
-        print(siteid_name)
-        
-        # Get sensor_id and unit_id
-        sensor_info = get_sensorinfo(siteid, varname)
-        
-        # add a sitename column to sensor_info by using siteid_name as mapping (column 0 is site_id, and column 1 is site_name)
-        for i, row in enumerate(sensor_info):
-            site_id = row['site_id']
-            # Find the corresponding site name from siteid_name
-            site_row = next((s for s in siteid_name if s[0] == site_id), None)
-            if site_row:
-                sensor_info[i]['site_name'] = site_row[1]
-            else:
-                sensor_info[i]['site_name'] = None
-        
-        # Add a fullname column to sensor_info by combining site_name and sensor_name
-        for i, row in enumerate(sensor_info):
-            site_name = row['site_name']
-            sensor_name = row['sensor_name']
-            # Combine site_name and sensor_name
-            sensor_info[i]['fullname'] = f"{site_name}_{sensor_name}"
-            
-        return sensor_info
-    
-    
     # Get the sensor_info by site and varname combination
-    sensor_info = get_sensorinfo_by_site_and_varname(sites, varname)
+    sensor_info = get_sensorinfo_by_site_and_varname(check_table)
         
     # make dataframe of sensor_info
     sensor_info_df = pd.DataFrame(sensor_info)
 
+    # Check if all items od check_table are in sensor_info_df
+    missing_items = check_table[~check_table.set_index(['Station', 'Variable']).index.isin(sensor_info_df.set_index(['site_name', 'sensor_name']).index)]
+    if not missing_items.empty:
+        print("The following items in the check_table are not found in the sensor_info:")
+        print(missing_items[['Station', 'Variable']])
+
     # Get the sensor_ids from sensor_info
     sensorids = [row['sensor_id'] for row in sensor_info]
     
-    
-
     # Get the sensor data from the database
     data = get_data(sensorids, start_dt, end_dt)
 
     # Pivot the DataFrame
     pivoted_df = data.pivot(index='dt', columns='logicid', values='value')
-
-
 
     # Add columns that are not in the pivoted_df but are in the sensor_info
     for row in sensor_info:
@@ -274,8 +360,8 @@ if __name__ == '__main__':
     # Rename the columns in pivoted_df using the mapping
     pivoted_df.rename(columns=sensor_id_to_name, inplace=True)
 
-    print(sensor_info_df)
-    print(pivoted_df)
+    # print(sensor_info_df)
+    # print(pivoted_df)
 
     # make a plotly plot of the data
     import plotly.express as px
